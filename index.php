336b3fcc4241d80d635072dec1a7a2071b2d48d9
Greengerty <greengerty@gmail.com>
2016-03-10 10:01:58 +0200

    add action loggin

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 require_once "startup.php";
-
 require_once "controllers/UserController.php";
+
+if(isset($_GET['page'])) {
+    $page = $_GET['page'];
+
+    if($page == "auth") {
+        $username = $_POST['username'];
+        login($conn, $username);
+    }
+}
+
+view_login();
